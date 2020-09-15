d73c4d651f447a7917e0ecc6a1901b1df84330f6
Victor Truong <victor.truong@learn.cssd.ab.ca>
2020-09-15 19:02:55 +0000

    Initial commit

---
+++ index.java
@@ -0,0 +1,21 @@
+//global variables
+int location = 450;
+
+void setup() {
+	size(500, 500);
+}//end setup()
+
+void draw() {
+	background(6,18,100);
+	println("Hello World!");
+  String firstName = "Bobby";
+  println("Hello " + firstName);
+  firstName = "Jill";
+  println("hi " + firstName);
+  textSize(20);
+  text("welcome to com sci",location,100);
+  location = location - 10;
+  if(location < -50){
+    l
+  }//end if
+}//end draw();
